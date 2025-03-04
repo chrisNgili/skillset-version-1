b95efd65e0c53060a7971b69a8affcc25ee5fef8
chrisngili <ngilichristopher@gmail.com>
2025-03-04 20:49:04 +0300

    javscript

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const input = document.getElementById('button');
 input.addEventListener('click', function() {
   alert('Wupps! Nothing here');
 });
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", function(event) {
+      event.preventDefault(); 
+      const targetId = this.getAttribute("href");
+      document.querySelector(targetId).scrollIntoView({ behavior: "smooth" });
+  });
+});
